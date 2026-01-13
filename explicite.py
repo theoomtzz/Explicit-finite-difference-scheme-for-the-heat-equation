0ee01e8d35102bf63e6be28bd1168ea68c0e264a
Théo METTEZ <theomettez@Theos-MacBook-Air.local>
2026-01-13 10:21:41 +0100

    Parameters Initialisations

--- explicite.py
+++ explicite.py
@@ -1,7 +1,45 @@
-# Initialisation of phyiscal parameters
+import numpy as np
 
-# Difusivity
+##  Initialisation of phyiscals parameters
+
+# Total depth (meters)
+L = 100
+
+# Total time of one year (in seconds)
+S = 365 * 24 * 60 * 60
+
+# Dimension of the meshing
+Nz = 400
+Nt = 5000
+
+# Initialisation of meshing
+data = np.zeros(shape = (Nt, Nz))
+
+# Z axis (depth)
+Z = np.linspace(0, L, Nz)
+
+# T axis (time)
+T = np.linspace(0, S, Nt)
+
+# Initiale condition (temperature surface approximation)
+def Uini(t):
+    res = 15 - 10 * np.sin((2 * np.pi * t) / 12)
+    return res
+
+# Finale condition 
+def Ufin(t,z):
+    res = 0
+    return res
+
+# Pas d'espace 
+dz = L / Nz
+
+# Pas de temps 
+dt = S / Nt
+
+# Coeff of difusivity
 K = 10**-6
+
 
 
 
