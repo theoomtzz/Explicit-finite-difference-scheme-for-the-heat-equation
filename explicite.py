a852fd9d3e72d4b8aac1d8d417155dca2238d4ae
Théo METTEZ <theomettez@Theos-MacBook-Air.local>
2026-01-13 14:54:29 +0100

    third part probleme advanced

--- explicite.py
+++ explicite.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time 
 ##  Initialisation of phyiscals parameters
 
 # Total depth (meters)
@@ -12,8 +12,8 @@ S = 365 * 24 * 60 * 60
 Nz = 400
 Nt = 5000
 
-# Initialisation of meshing
-data = np.zeros(shape = (Nt, Nz))
+# Initialisation of meshing (15 degres celsiuce everywhere)
+U = np.ones(shape = (Nt, Nz)) * 15
 
 # Z axis (depth)
 Z = np.linspace(0, L, Nz)
@@ -41,5 +41,40 @@ dt = S / Nt
 K = 10**-6
 
 
+## Calcul par differences finies
 
+# Timer
+start_time = time.time()
 
+# Calcul de U à la surface pour tout les t
+U[:, 0] = Uini(T)
+
+# Condition d'arret si non convergence
+maxiter = 500
+
+# Sauvegarde de la dernière temprerature calculée
+Uold = U[:,:]
+
+err = 1
+count = 0
+
+# On met a jour chaque année la température initial avec la temperature final de l'an précédent
+# Obtenir un 
+while err > 10**-4:
+    # Calcul de U au temps suivant pour chaque profondeur
+    for i in range (1, Nt):
+        
+        a = (K * dt / (dz**2)) * (U[i-1, 0: -2])
+        U[i, 1: -1] = U[i-1, 1: -1] + (K * dt / (dz**2)) * (U[i-1, 0: -2] - 2 * U[i-1, 1: -1] + U[i-1, 2:])
+    count +=1
+    if count > 500:
+        print("Instable solution")
+        StopIteration
+    # Calcul de U au temps final
+    # U[-1, :] = Ufin()
+# End timer
+end_time = time.time()
+
+total_time = start_time - end_time
+
+print("Total time execution: ", total_time)
